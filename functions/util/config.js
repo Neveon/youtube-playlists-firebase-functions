7833aef400811f60557cc8325b92f1e3e6f58916
Neil T <neiltz711@gmail.com>
2019-07-31 13:26:52 -0400

    Update for public branch

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: 'AIzaSyBcGHDJoX5K-y1cK7wlyPPt5elHQ4H7IRQ',
-  authDomain: 'playlist-16862.firebaseapp.com',
-  databaseURL: 'https://playlist-16862.firebaseio.com',
-  projectId: 'playlist-16862',
-  storageBucket: 'playlist-16862.appspot.com',
-  messagingSenderId: '427214739068',
-  appId: '1:427214739068:web:af572aca1a8803ce'
+  apiKey: 'API_KEY_HERE',
+  authDomain: 'PROJECT_AUTH_DOMAIN',
+  databaseURL: 'PROJECT_DATABASE_URL',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'PROJECT_STORAGE_BUCKET',
+  messagingSenderId: 'PROJECT_MESSAGING_SENDER_ID',
+  appId: 'PROJECT_APP_ID'
 };
